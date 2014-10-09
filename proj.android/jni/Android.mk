4121446967810fad3ad03b172c1a5f8094e86374
Jeisson Hidalgo <jeissonh@gmail.com>
2014-10-09 10:54:09 -0600

    Project source files reorganized

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -12,8 +12,8 @@ LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
-                   ../../Classes/BaseScene.cpp \
-                   ../../GameMenu/GameMenuScene.cpp
+                   ../../Classes/Common/BaseScene.cpp \
+                   ../../Classes/GameMenuScene.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
